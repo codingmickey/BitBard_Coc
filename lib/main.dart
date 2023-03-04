6e146f57915ad3a9b3941c98c4c7f734bb1cc500
Yashs1623 <yashs230602@gmail.com>
2023-03-04 12:31:44 +0530

    Intial Commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:trinity/splash_screen.dart';
+import 'package:trinity/utils/constants.dart';
+
+// Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   await Firebase.initializeApp();
+//   print("Handling a background message: ${message.messageId}");
+// }
+
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  // flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  // await flutterLocalNotificationsPlugin
+  //     .resolvePlatformSpecificImplementation<
+  //         AndroidFlutterLocalNotificationsPlugin>()
+  //     ?.createNotificationChannel(channel);
+  // await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+  //     alert: true, badge: true, sound: true);
+  await GetStorage.init();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]);
+  runApp(const MyApp());
+}
+
+// const AndroidNotificationChannel channel = AndroidNotificationChannel(
+//     'high importance channel', 'High Importance Notifications',
+//     description: 'This channel is used for important Notifications',
+//     importance: Importance.high);
+// final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//     FlutterLocalNotificationsPlugin();
+// InitializationSettings initializationSettings =
+//     InitializationSettings(android: AndroidInitializationSettings('logo'));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: kPrimaryColor,
+              fontFamily: 'Montserrat',
+            ),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
